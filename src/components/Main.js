4b41dea38137ac78f924083886c86587eee3459c
MiRECoFu <1027066037@qq.com>
2017-01-10 16:16:05 +0800

    基本界面

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,16 +2,53 @@ require('normalize.css/normalize.css');
 require('styles/App.css');
 
 import React from 'react';
+import ReactDOM from 'react-dom';
+//let yeomanImage = require('../images/yeoman.png');
 
-let yeomanImage = require('../images/yeoman.png');
+//单个章节节点组件
+class ChapIndexs extends React.Component {
+  render() {
+
+    //设置此组件大小
+  //  let chapClassName = 'chap-name';
+
+    return (
+      <div className="chap" ref="chap">
+
+      </div>
+    );
+  }
+}
+
+
 
 class AppComponent extends React.Component {
   render() {
+
+    let chapIndex = [];
+
+    chapIndex.push(<ChapIndexs key=""/>);
+
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      <section className="workplace" ref="workplace">
+        <section className="controllnav">
+
+              <section className="chaps" ref="chaps">
+                {chapIndex}
+                <button className="newchap" ref="newchap"> + </button>
+              </section>
+
+        </section>
+
+        <section className="editplace">
+
+        </section>
+
+
+        <section className="tipbar">
+
+        </section>
+      </section>
     );
   }
 }
