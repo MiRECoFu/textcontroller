31a1ae7f1c13cd37a092cfacb1a05465123cfd9b
MiRECoFu <1027066037@qq.com>
2017-01-11 17:45:46 +0800

    Revert "点击添加"
    This reverts commit c689464c8f2ad0809b64769b6a149f5bb46dd50d.

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,58 +5,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //let yeomanImage = require('../images/yeoman.png');
 
-
-//拿到储存章节信息的json文件，转化为数组
-//let chapterDatas = require('../data/chapterData.json');
-
-let chapterDatas = [
-  {
-    'chapterIndex':1,
-    'chapterName':''
-  }
-];
-
 //单个章节节点组件
 class ChapIndexs extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-              add: false,
-              delete: false
-            };
-  }
-  //点击为chapterDatas数组添加新的一组章节数据项
-    _add(){
-      chapterDatas.push({
-        'chapterIndex':chapterDatas.length + 1,
-        'chapterName':''
-      });
-      console.log(chapterDatas.indexOf(chapterDatas.length).chapterIndex);
-      this.setState({
-        add: true
-      })
-    }
-
   render() {
 
+    //设置此组件大小
+  //  let chapClassName = 'chap-name';
 
     return (
-      <div>
-        {
-          chapterDatas.map((name)=>{
-            let key = chapterDatas.indexOf(name);
-            return(
-              <div className="chap" ref={'chap'+name} key={key}>
-                <span>Chapter{key+1}</span>
-              </div>
-            );
-          })
-        }
-        <input  className="newchap"
-                type="button"
-                value="+"
-                onClick={this._add.bind(this)}
-              />
+      <div className="chap" ref="chap">
+
       </div>
     );
   }
@@ -65,17 +23,11 @@ class ChapIndexs extends React.Component {
 
 
 class AppComponent extends React.Component {
-
-
-
   render() {
 
-  let chapIndex = [];
+    let chapIndex = [];
 
-  //  chapIndex.push(<ChapIndexs name=""/>);
-  for(let i=0;i<=chapterDatas.length;i++) {
-    chapIndex.push(<ChapIndexs name="" key=""/>)
-  }
+    chapIndex.push(<ChapIndexs key=""/>);
 
     return (
       <section className="workplace" ref="workplace">
@@ -83,6 +35,7 @@ class AppComponent extends React.Component {
 
               <section className="chaps" ref="chaps">
                 {chapIndex}
+                <button className="newchap" ref="newchap"> + </button>
               </section>
 
         </section>
