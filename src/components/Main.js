e894f4433541b9ea004be58193189f3135001e11
themachine15 <aa674072911@hotmail.com>
2017-01-16 20:50:19 +0800

    副本

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,17 +4,18 @@ require('styles/App.css');
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BosClient} from 'bce-sdk-js';
+import {
+  BosClient
+} from 'bce-sdk-js';
 
 import AMUIReact from 'amazeui-react';
 var Button = AMUIReact.Button;
-var Icon = AMUIReact.Icon;
-
+// var Icon = AMUIReact.Icon;
 const config = {
-  endpoint: 'http://gz.bcebos.com',         //传入Bucket所在区域域名
+  endpoint: 'http://gz.bcebos.com', //传入Bucket所在区域域名
   credentials: {
-      ak: '6bb013b74f43441aa2961130a550837d',         //您的AccessKey
-      sk: '114576782dd448b89c8e4b2b2db551be'       //您的SecretAccessKey
+    ak: '6bb013b74f43441aa2961130a550837d', //您的AccessKey
+    sk: '114576782dd448b89c8e4b2b2db551be' //您的SecretAccessKey
   }
 };
 
@@ -38,96 +39,109 @@ class ChapIndexs extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-              add: false,
-              delete: false,
-              clicked:false,
-              nameChange: false
-            };
+      addCont: chapterDatas.length,
+      clicked: false,
+      nameChange: false
+    };
   }
+
   //点击为chapterDatas数组添加新的一组章节数据项
-    _add(e){
-      chapterDatas.push({
-        chapterIndex:chapterDatas.length + 1,
-        chapterName:''
-      });
 
-
-      console.log(chapterDatas[chapterDatas.length-1]);
-      //取出每个章节信息的index
-      let chapi = [];
-      chapterDatas.forEach(function(v){chapi.push(v.chapterIndex)});
-      console.log(chapi[chapi.length-1]);
-
-      this.setState({
-        add: true
+  _add() {
+    let count = this.state.addCont;
+    count++;
+    this.setState({
+        addCont: count,
       })
+      // console.log(count + 'add')
+    chapterDatas.push({
+      chapterIndex: chapterDatas.length + 1,
+      chapterName: chapterDatas.length + 1
+    });
 
-      e.stopPropagation();
-      e.preventDefault();
-    }
+  }
 
 
   //点击删除某一章节节点信息
-    _delete(index){
-          chapterDatas.splice(chapterDatas.indexOf(index),1);
-          this.setState({
-            delete: true
-          })
-        }
-
-    _onClick(){
-      this.setState({
-        clicked:true
+  _delete(obj) {
+    let count = this.state.addCont;
+    count--;
+    this.setState({
+        addCont: count
       })
-    }
+      // console.log(count + 'delete')
+    chapterDatas.splice(chapterDatas.indexOf(obj), 1);
+  }
 
-    _onChange(key){
-      //取出每个章节信息的Name
+  //创建副本
+  _createBre(obj) {
+    let count = this.state.addCont;
+    count++;
+    this.setState({
+        addCont: count,
+      })
+      // console.log(count + 'create')
+    let proObj = chapterDatas.slice(chapterDatas.indexOf(obj), chapterDatas.indexOf(obj) + 1)
+    chapterDatas.splice(chapterDatas.indexOf(obj) + 1, 0, {
+      chapterIndex: chapterDatas.length + 1,
+      chapterName: proObj[0].chapterName + '副本',
+    });
 
-      chapterDatas[key].chapterName = this.refs['cNameValue'+key].value;
+    // this._add()
+  }
 
-      console.log(chapterDatas[key]);
+  _onClick() {
+    this.setState({
+      clicked: true
+    })
+  }
 
-      this.setState({
-        nameChange: true
-      });
-    }
+  _onChange(key) {
+    //取出每个章节信息的Name
+
+    chapterDatas[key].chapterName = this.refs['cNameValue' + key].value;
+
+    this.setState({
+      nameChange: true
+    });
+  }
 
   render() {
 
     return (
       <div>
+        <Button  
+          className="newchap"
+          onClick = {
+           this._add.bind(this)
+          }
+        >
+          +
+        </Button>
         {
-          chapterDatas.map((name)=>{
-            let key = chapterDatas.indexOf(name);
+          chapterDatas.map((data)=>{
+            let key = chapterDatas.indexOf(data);
+            let name = data.chapterName;
+            
             return(
               <div className='chap'  key={key}>
-                <span>Chapter{key+1} :</span>
-                <span>{chapterDatas[key].chapterName}</span>
+                <span>Chapter{key+1}: </span>
+                <span>{name}</span>
                 <div>{'章节简介：'+chapterDatas[key].chapterSum}</div>
-
-                <Button className="setting">编辑</Button>
-
+                <Button amSize="sm" className="setting">编辑</Button>
                 <div className="editChapP">
-
-
-                      <Button onClick={()=>{this._delete(name)}}>删除</Button>
-
-
-                      <input
-                      ref={'cNameValue'+key}
-                      type="text"
-                      onChange={()=>{this._onChange(key)}}
-                      />
-
+                  <Button  amSize="sm" onClick={()=>{this._delete(data)}}>删除</Button>
+                  <Button  amSize="sm" onClick={()=>{this._createBre(data)}}>创建副本</Button>
+                  <input
+                  ref={'cNameValue'+key}
+                  type="text"
+                  onChange={()=>{this._onChange(key)}}
+                  />
               </div>
               </div>
             );
           })
         }
-        <Button  className="newchap"
-                onClick={this._add.bind(this)}
-              >+</Button>
       </div>
     );
   }
@@ -137,51 +151,23 @@ class ChapIndexs extends React.Component {
 
 class AppComponent extends React.Component {
 
-
-
   render() {
-
-
     return (
       <section className="workplace" ref="workplace">
         <section className="controllnav">
-
-              <Button>
-                <Icon icon="cog" /> 设置
-              </Button>
-
-              <Button amStyle="warning">
-                <Icon icon="shopping-cart" /> 败家
-              </Button>
-
-              <Button>
-                <Icon icon="spinner" spin /> Loading
-              </Button>
-              <div>
-                  <p><Icon icon="qq" /> QQ</p>
-                  <p><Icon icon="weixin" /> Wechat</p>
-              </div>
-
-              <section className="chaps" ref="chaps">
-                <ChapIndexs/>
-              </section>
-
+          <section className="chaps" ref="chaps">
+            <ChapIndexs/>
+          </section>
         </section>
-
         <section className="editplace">
-
         </section>
-
-
         <section className="tipbar">
-
         </section>
       </section>
     );
   }
 }
 
-AppComponent.defaultProps = {
-};
+AppComponent.defaultProps = {};
 
 export default AppComponent;
