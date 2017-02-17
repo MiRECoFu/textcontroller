f123b1d71830c4484a12b94344c9e1e8e7b34b9c
JasonSi <529511542@qq.com>
2017-02-17 13:50:18 +0800

    新增添加文档功能；提供回收站支持

--- src/components/Studio.js
+++ src/components/Studio.js
@@ -13,7 +13,17 @@ var Icon = AMUIReact.Icon;
 var Storage = require('../storage/storage.js');
 
 let presetDatas = require('../data/docData.json');
+let myDocs = [];
+let deletedDocs = [];
+for(let i in presetDatas){
+  if(presetDatas[i].deleted){
+    deletedDocs.push(presetDatas[i]);
+  } else {
+    myDocs.push(presetDatas[i]);
+  }
+}
 
+console.log(myDocs);
 //获取用户信息
 //获取 img src
 
@@ -29,7 +39,20 @@ class Studio extends React.Component {
 
   //新建
   _newItem() {
-
+    let date = new Date();
+    let time = date.getFullYear().toString() + (date.getMonth() + 1).toString() + date.getDate();
+    time += date.getTime().toString().substr(11);
+    let newDoc = {
+      total: 0,
+      title: "未命名",
+      id: time.substr(4),
+      data: []
+    }
+    myDocs.push(newDoc);
+    this.setState({
+      docsCount: myDocs.length
+    })
+    Storage.set('docs', myDocs);
   }
 
   //
@@ -47,18 +70,19 @@ class Studio extends React.Component {
         </div>
         <div className="main">
           <div className="nav">
-            <div 
+            <div
               className="new-doc"
               onClick={()=>{this._newItem()}}
             >新建</div>
             <ul>
+              <li><Link to='/studio'>我的文档</Link></li>
               <li><a href="#">协作消息</a></li>
               <li><a href="#"><Icon icon="trash" /> 回收站</a></li>
             </ul>
           </div>
           <div className="content">
             {
-              presetDatas.map(function(data, i){
+              myDocs.map(function(data){
                 let docUrl = "/studio/docs/" + data.id;
                 return (<Link to={docUrl} key={data.id} className="doc-item">{data.title || "未命名"}</Link>);
               })
